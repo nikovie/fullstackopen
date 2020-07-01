376c57408b8d2d04226b099ba03d39b36b22cefe
Niko Vierikko <niko>
2020-07-01 15:18:36 +0300

    Task 4.8 bloglist tests - express app isolated from index.js - separate configs for dev&testing - new dependencies

--- osa4/index.js
+++ osa4/index.js
@@ -1,17 +1,9 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
