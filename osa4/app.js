61e4a151c10e01cb83685a1b61aa9d78da88b04e
Niko Vierikko <niko>
2020-07-07 00:11:36 +0300

    Better error handling

--- osa4/app.js
+++ osa4/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -11,5 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ osa4/utils/middleware.js
@@ -0,0 +1,17 @@
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.log('error', error)
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler
+}
