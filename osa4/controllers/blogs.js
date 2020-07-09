5d206d32c490e2e6df0467a91007d21058d47fa8
Niko Vierikko <61685032+nikovie@users.noreply.github.com>
2020-07-09 23:22:52 +0300

    Task 4.19

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -21,6 +30,7 @@ blogRouter.get('/:id', async (request, response, next) => {
 
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const token = getTokenFrom(request)
 
   if (!body.title || !body.url) {
     return response.status(400).json({
@@ -29,7 +39,13 @@ blogRouter.post('/', async (request, response, next) => {
   }
 
   try {
-    const user = await User.findOne({})
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
