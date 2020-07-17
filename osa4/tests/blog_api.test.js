fd61720dca67468ab5a7a8c51579780bcf6dc021
Niko Vierikko <niko@camoline.fi>
2020-07-17 22:55:04 +0300

    Task 4.22

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -5,6 +7,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const blogsForTesting = [
   {
@@ -22,12 +25,21 @@ const blogsForTesting = [
 ]
 
 beforeEach(async () => {
+  await User.deleteMany({})
+
+  const passwordHash = await bcrypt.hash('nananana', 10)
+  const user = new User({ username: 'root', passwordHash })
+  
+  await user.save()
+
   await Blog.deleteMany({})
 
   let blogObj = new Blog(blogsForTesting[0])
+  Object.assign(blogObj, { user: user._id})
   await blogObj.save()
 
   blogObj = new Blog(blogsForTesting[1])
+  Object.assign(blogObj, { user: user._id})
   await blogObj.save()
 })
 
@@ -58,15 +70,28 @@ describe('get blogs', () => {
 
 describe('addition of new blogs', () => {
   test('new blogs can be added and the number of blogs increases', async () => {
+    const passwordHash = await bcrypt.hash('allmightykey', 10)
+    const user = new User({ username: 'blogger', name: 'Bob The Blogger', passwordHash})
+    await user.save()
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+  
+    const accessToken = jwt.sign(userForToken, process.env.SECRET)
+
     const newBlog = {
       title: "Testing addition of new blog",
       author: "Blog Api Tester",
       url: "www",
-      likes: 99
+      likes: 99,
+      user: user._id
     }
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${accessToken}`)
       .send(newBlog)
       .expect(200)
     
@@ -74,14 +99,27 @@ describe('addition of new blogs', () => {
     expect(blogs.body.length).toBe(blogsForTesting.length + 1)
   })
   test('adding new blog w/o likes returns 0', async () => {
+    const passwordHash = await bcrypt.hash('allmightykey', 10)
+    const user = new User({ username: 'blogger', name: 'Bob The Blogger', passwordHash})
+    await user.save()
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+  
+    const accessToken = jwt.sign(userForToken, process.env.SECRET)
+
     const newBlog = {
       title: "A new blog with 0 likes",
       author: "Blog Api Tester",
-      url: "www"
+      url: "www",
+      user: user._id
     }
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${accessToken}`)
       .send(newBlog)
       .expect(200)
     
@@ -91,22 +129,55 @@ describe('addition of new blogs', () => {
     expect(newest).toBe(0)
   })
   test('adding new blog w/o title and/or url returns 400 bad request', async () => {
+    const passwordHash = await bcrypt.hash('allmightykey', 10)
+    const user = new User({ username: 'blogger', name: 'Bob The Blogger', passwordHash})
+    await user.save()
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+  
+    const accessToken = jwt.sign(userForToken, process.env.SECRET)
+
     const newBlog = {
       author: "Blog Api Tester"
     }
 
     await api
       .post('/api/blogs')
+      .set('Authorizaton', `Bearer ${accessToken}`)
       .send(newBlog)
       .expect(400)
+  })
+  test('cannot login with wrong crendentials', async () => {
+    const user = {
+      username: 'foo', 
+      password: 'bar'
+    }
+
+    const login = await api
+      .post('/api/login')
+      .send(user)
+      .expect(401)
   })
 })
 describe('remove blogs', () => {
   test('remove blog by id', async () => {
     const blogs = await api.get('/api/blogs')
-    
+
+    const login = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'nananana'})
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const accessToken = login.body.token
+    expect(accessToken).toBeDefined()
+
     await api
       .delete(`/api/blogs/${blogs.body[0].id}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .expect(204)
 
     const blogsAfter = await api.get('/api/blogs')
