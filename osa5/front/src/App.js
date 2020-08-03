7febde08584581f568194b904dd625e727601bb9
Niko Vierikko <niko@camoline.fi>
2020-08-03 15:23:15 +0300

    Tasks 5.2, 5.4

--- osa5/front/src/App.js
+++ osa5/front/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notication'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -15,6 +17,23 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    console.log('loggedUserJSON', loggedUserJSON)
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const notifyWith = (message, type='success') => {
+    setMessage({ message, type })
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -23,50 +42,96 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     }
     catch (exception) {
-      console.log('err')
+      notifyWith('Wrong credentials', 'error')
     }
   }
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-        <div>
-          Username: 
-          <input 
-            type="text"
-            value={username}
-            name="username"
-            onChange={({target}) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          Password: 
-          <input 
-            type="password"
-            value={password}
-            name="password"
-            onChange={({target}) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      </div>
-    )
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
-  return (
-    <div>
-      <h2>blogs</h2>
+  const loginForm = () => (
+    <>
+    <form onSubmit={handleLogin}>
+      <div>
+        Username: 
+        <input 
+          type="text"
+          value={username}
+          name="username"
+          onChange={({target}) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        Password: 
+        <input 
+          type="password"
+          value={password}
+          name="password"
+          onChange={({target}) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    </>
+  )
+  
+  const blogList = () => (
+    <>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </>
+  )
+
+        <div>
+            type="text"
+          />
+        </div>
+        <div>
+          />
+        </div>
+      </form>
+
+  return (
+    <div>
+      <div className="bg-gray ph2">
+        <div className="dib">
+          <h1>Blogs</h1>
+
+          {user !== null && 
+            <button onClick={() => handleLogout()}>Add new</button>
+          }
+        </div>
+        <div className="dib fr">
+          {user !== null && 
+            <p>{user.name} logged in <button onClick={() => handleLogout()}>logout</button></p>
+          }
+        </div>
+      </div>
+
+      <Notification notification={message}/>
+
+      <div className="ma2">
+        {user === null ?
+          loginForm() :
+          <div>
+            {blogList()}
+          </div>
+        }
+      </div>
     </div>
   )
 }
---
+++ osa5/front/src/components/Notication.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Notification = ({notification}) => {
+  const defaultCss = 'ma2 pa2 bw2 b--solid'
+  
+  if (notification === null) {
+    return null
+  }
+
+  if (notification.type === 'error') {
+    notification.type = 'b--red'
+  } else if  (notification.type === 'success') {
+    notification.type = 'b--green'
+  } 
+
+  return (
+    <div className={`${defaultCss} ${notification.type}`}>
+      {notification.message}
+    </div>
+  )
+}
+
+export default Notification
