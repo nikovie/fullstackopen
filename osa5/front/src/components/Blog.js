8c67a93e7e289905fe51fba432f7c6d3c015d3af
Niko Vierikko <niko@camoline.fi>
2020-08-07 15:45:53 +0300

    Task 5.7

--- osa5/front/src/components/Blog.js
+++ osa5/front/src/components/Blog.js
@@ -2,15 +2,6 @@ import React, {useState} from 'react'
 import Togglable from './Togglable'
 
 const Blog = ({ blog }) => {
-  const [details, showDetails] = useState('')
-
-  const min = (id) => {
-    const details = `nanan ${id}`
-    return (
-      <p>{details}</p>
-    )
-  }
-
   return (
     <div className="mb2 ph1 pv2 b--solid b--gray">
       <div>
