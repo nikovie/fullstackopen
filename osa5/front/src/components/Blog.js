6f392a0049c8ee6ccddc0c17ccb576809d56484b
Niko Vierikko <niko@camoline.fi>
2020-09-09 13:49:47 +0300

    Task 5.7 - didn't belive that toggable-component doesn't work in here

--- osa5/front/src/components/Blog.js
+++ osa5/front/src/components/Blog.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import Togglable from './Togglable'
 import blogs from '../services/blogs'
 
 const Blog = ({ blog, likeBlog, removeBlog, user }) => {
+  const [details, showDetails] = useState(false)
+
+  const showMore = () => {
+    showDetails(!details)
+  }
+
   const handleLike = () => {
     blogs.likes === null ? blog.likes=0 : blog.likes++
     likeBlog(blog)
@@ -12,29 +17,33 @@ const Blog = ({ blog, likeBlog, removeBlog, user }) => {
   const handleRemove = () => {
     removeBlog(blog.title, blog.id)
   }
-
   return (
     <div className="mb2 ph1 pv2 b--solid b--gray">
       <div>
         {blog.title}
         <i> by {blog.author}</i>
       </div>
-      <Togglable showLabel="view">
-        <div>{blog.url}</div>
-        <div>
-          Likes: {blog.likes === null ? 0 : `${blog.likes}`}
-          <button onClick={handleLike}>Like</button>
-        </div>
-        {blog.user.username === user.username && <button className="fr" onClick={handleRemove}>Remove</button>}
-      </Togglable>
+      {details ? (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            Likes: {blog.likes === null ? 0 : `${blog.likes}`}
+            <button onClick={handleLike}>Like</button>
+          </div>
+          {blog.user && blog.user.username === user.username && <button className="fr" onClick={handleRemove}>Remove</button>}
+          <button onClick={() => showMore()}>hide</button>
+        </>
+      ) : (
+        <button onClick={() => showMore()}>view</button>
+      )}
     </div>
   )
 }
 
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
-  likeBlog: PropTypes.func.isRequired,
-  removeBlog: PropTypes.func.isRequired,
+  likeBlog: PropTypes.func,
+  removeBlog: PropTypes.func,
   user: PropTypes.object.isRequired
 }
 
