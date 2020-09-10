68f68f63787e7fa4079933ee559612981f550d1f
Niko Vierikko <niko@camoline.fi>
2020-09-10 08:36:00 +0300

    Task 5.15

--- osa5/front/src/components/Blog.test.js
+++ osa5/front/src/components/Blog.test.js
@@ -3,53 +3,60 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author by default but not other info', () => {
-  const blog = {
-    title: 'Sample title',
-    author: 'Bob the Bot',
-    url: 'www.fi',
-    likes: '100'
-  }
-  const user = {
-    username: 'bot',
-    password: 'top'
-  }
+describe('<Blog />', () => {
+  let component
+  let mockHandler
 
-  const component = render(
-    <Blog blog={blog} user={user} />
-  )
+  beforeEach(() => {
+    const blog = {
+      title: 'Sample title',
+      author: 'Bob the Bot',
+      url: 'www.fi',
+      likes: '100'
+    }
+    const user = {
+      username: 'bot',
+      password: 'top'
+    }
+    
+    mockHandler = jest.fn()
 
-  // component.debug()
+    component = render(
+      <Blog blog={blog} user={user} likeBlog={mockHandler} />
+    )
+  })
 
-  expect(component.container).toHaveTextContent('Sample title')
-  expect(component.container).toHaveTextContent('Bob the Bot')
-  expect(component.container).not.toHaveTextContent('www.fi')
-  expect(component.container).not.toHaveTextContent('100')
-})
-
-test('all details are shown when view button is clicked', () => {
-  const blog = {
-    title: 'Sample title',
-    author: 'Bob the Bot',
-    url: 'www.fi',
-    likes: '100'
-  }
-  const user = {
-    username: 'bot',
-    password: 'top'
-  }
-
-  const component = render(
-    <Blog blog={blog} user={user} />
-  )
-
-  const button = component.getByText('view')
-  fireEvent.click(button)
-
-  // component.debug()
-
-  expect(component.container).toHaveTextContent('Sample title')
-  expect(component.container).toHaveTextContent('Bob the Bot')
-  expect(component.container).toHaveTextContent('www.fi')
-  expect(component.container).toHaveTextContent('Likes: 100')
+  test('renders title and author by default but not other info', () => {
+    // component.debug()
+  
+    expect(component.container).toHaveTextContent('Sample title')
+    expect(component.container).toHaveTextContent('Bob the Bot')
+    expect(component.container).not.toHaveTextContent('www.fi')
+    expect(component.container).not.toHaveTextContent('100')
+  })
+  
+  test('all details are shown when view button is clicked', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+  
+    // component.debug()
+  
+    expect(component.container).toHaveTextContent('Sample title')
+    expect(component.container).toHaveTextContent('Bob the Bot')
+    expect(component.container).toHaveTextContent('www.fi')
+    expect(component.container).toHaveTextContent('Likes: 100')
+  })
+  
+  test('likeBlog handler are call as many times like button is clicked', () => {
+    const viewButton = component.getByText('view')
+    fireEvent.click(viewButton)
+  
+    const likeButton = component.getByText('Like')
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+  
+    // component.debug()
+  
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
 })
