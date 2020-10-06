5a420bebec7e78d142b1bdc74c700a2b9e727d8f
Niko Vierikko <niko@camoline.fi>
2020-10-06 23:00:00 +0300

    Task 7.16

--- osa7/bloglist/front/src/components/Header.js
+++ osa7/bloglist/front/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { userLogout } from '../reducers/userReducer'
 
 const Header = ({ title, user }) => {
   const dispatch = useDispatch()
-
   return (
     <div className="bg-gray ph2">
       <div className="dib">
@@ -14,6 +14,12 @@ const Header = ({ title, user }) => {
         {user !== null &&
           <p>{user.name} logged in <button onClick={() => dispatch(userLogout())}>logout</button></p>
         }
+      </div>
+      <div className="menu">
+      <nav>
+        <Link to="/" className="menuItem">Blogs</Link>
+        <Link to="/users" className="menuItem">Users</Link>
+      </nav>
       </div>
     </div>
   )
